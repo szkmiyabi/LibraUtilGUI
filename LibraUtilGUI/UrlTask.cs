eb080382c52a511ad6c3966ac7e4a49ca68e3417
szkmiyabi <led.centipede@gmail.com>
2020-02-11 19:27:19 +0900

    UrlTaskの挙動を修正（積み残し分）
    Excel出力とPIDのみ出力もページID絞り込みに対応

--- LibraUtilGUI/UrlTask.cs
+++ LibraUtilGUI/UrlTask.cs
@@ -118,13 +118,19 @@ namespace LibraUtilGUI
             Task.Run(() =>
             {
 
-                d_status_messenger message = new d_status_messenger(w_status_messenger);
-                d_get_workDir _d_get_workDir = new d_get_workDir(w_get_workDir);
-                d_get_projectID _d_get_projectID = new d_get_projectID(w_get_projectID);
-                d_get_UrlTask_source_flag _d_get_UrlTask_source_flag = new d_get_UrlTask_source_flag(w_get_UrlTask_source_flag);
+                //共通デリゲートインスタンス
+                d_status_messenger message = w_status_messenger;
+                d_ldr_activate ldr_activate = w_ldr_activate;
+                d_task_cancel canceler = w_task_cancel;
 
-                d_ldr_activate ldr_activate = new d_ldr_activate(w_ldr_activate);
-                d_task_cancel canceler = new d_task_cancel(w_task_cancel);
+                //専用デリゲートインスタンス（条件系）
+                d_get_UrlTask_source_flag _get_UrlTask_source_flag = w_get_UrlTask_source_flag;
+                d_is_pageID_selected _is_pageID_selected = w_is_pageID_selected;
+
+                //専用デリゲートインスタンス（取得系）
+                d_get_workDir _get_workDir = w_get_workDir;
+                d_get_projectID _get_projectID = w_get_projectID;
+                d_pageID_data get_page_rows = w_pageID_data;
 
                 if (ldr_activated == false)
                 {
@@ -137,10 +143,10 @@ namespace LibraUtilGUI
                 ldr.login();
                 DateUtil.app_sleep(shortWait);
 
-                string projectID = (string)this.Invoke(_d_get_projectID);
+                string projectID = (string)this.Invoke(_get_projectID);
                 ldr.projectID = projectID;
 
-                string flag = (string)this.Invoke(_d_get_UrlTask_source_flag);
+                string flag = (string)this.Invoke(_get_UrlTask_source_flag);
                 List<List<string>> data = new List<List<string>>();
                 string site_name = "";
 
@@ -155,6 +161,25 @@ namespace LibraUtilGUI
 
                     site_name = ldr.get_site_name();
                     data = ldr.get_page_list_data();
+                }
+                else if (flag == "combo")
+                {
+                    if (!(Boolean)this.Invoke(_is_pageID_selected))
+                    {
+                        this.Invoke(message, "ページIDが選択されていません。処理を停止します。");
+                        ldr.logout();
+                        this.Invoke(message, "処理を停止しました。（" + DateUtil.get_logtime() + "）");
+                        return;
+                    }
+                    this.Invoke(message, "レポートインデックスページに移動します。（" + DateUtil.get_logtime() + "）");
+                    ldr.browse_repo();
+                    DateUtil.app_sleep(shortWait);
+
+                    //タスクのキャンセル判定
+                    if ((Boolean)this.Invoke(canceler)) return;
+
+                    site_name = ldr.get_site_name();
+                    data = (List<List<string>>)this.Invoke(get_page_rows);
                 }
                 else if (flag == "svpage")
                 {
@@ -179,7 +204,7 @@ namespace LibraUtilGUI
                 List<string> head_row = new List<string>() { "PID", "URL" };
                 data.Insert(0, head_row);
 
-                string save_dir = (string)this.Invoke(_d_get_workDir);
+                string save_dir = (string)this.Invoke(_get_workDir);
                 string save_filename = save_dir + projectID + "_" + site_name + " URL.xlsx";
 
                 //タスクのキャンセル判定
@@ -200,13 +225,19 @@ namespace LibraUtilGUI
             Task.Run(() =>
             {
 
-                d_status_messenger message = new d_status_messenger(w_status_messenger);
-                d_get_workDir _d_get_workDir = new d_get_workDir(w_get_workDir);
-                d_get_projectID _d_get_projectID = new d_get_projectID(w_get_projectID);
-                d_get_UrlTask_source_flag _d_get_UrlTask_source_flag = new d_get_UrlTask_source_flag(w_get_UrlTask_source_flag);
+                //共通デリゲートインスタンス
+                d_status_messenger message = w_status_messenger;
+                d_ldr_activate ldr_activate = w_ldr_activate;
+                d_task_cancel canceler = w_task_cancel;
 
-                d_ldr_activate ldr_activate = new d_ldr_activate(w_ldr_activate);
-                d_task_cancel canceler = new d_task_cancel(w_task_cancel);
+                //専用デリゲートインスタンス（条件系）
+                d_get_UrlTask_source_flag _get_UrlTask_source_flag = w_get_UrlTask_source_flag;
+                d_is_pageID_selected _is_pageID_selected = w_is_pageID_selected;
+
+                //専用デリゲートインスタンス（取得系）
+                d_get_workDir _get_workDir = w_get_workDir;
+                d_get_projectID _get_projectID = w_get_projectID;
+                d_pageID_data get_page_rows = w_pageID_data;
 
                 if (ldr_activated == false)
                 {
@@ -222,10 +253,10 @@ namespace LibraUtilGUI
                 //タスクのキャンセル判定
                 if ((Boolean)this.Invoke(canceler)) return;
 
-                string projectID = (string)this.Invoke(_d_get_projectID);
+                string projectID = (string)this.Invoke(_get_projectID);
                 ldr.projectID = projectID;
 
-                string flag = (string)this.Invoke(_d_get_UrlTask_source_flag);
+                string flag = (string)this.Invoke(_get_UrlTask_source_flag);
                 List<List<string>> data = new List<List<string>>();
                 string site_name = "";
 
@@ -240,6 +271,25 @@ namespace LibraUtilGUI
 
                     site_name = ldr.get_site_name();
                     data = ldr.get_page_list_data();
+                }
+                else if (flag == "combo")
+                {
+                    if (!(Boolean)this.Invoke(_is_pageID_selected))
+                    {
+                        this.Invoke(message, "ページIDが選択されていません。処理を停止します。");
+                        ldr.logout();
+                        this.Invoke(message, "処理を停止しました。（" + DateUtil.get_logtime() + "）");
+                        return;
+                    }
+                    this.Invoke(message, "レポートインデックスページに移動します。（" + DateUtil.get_logtime() + "）");
+                    ldr.browse_repo();
+                    DateUtil.app_sleep(shortWait);
+
+                    //タスクのキャンセル判定
+                    if ((Boolean)this.Invoke(canceler)) return;
+
+                    site_name = ldr.get_site_name();
+                    data = (List<List<string>>)this.Invoke(get_page_rows);
                 }
                 else if (flag == "svpage")
                 {
@@ -260,7 +310,7 @@ namespace LibraUtilGUI
                     if ((Boolean)this.Invoke(canceler)) return;
                 }
 
-                string save_dir = (string)this.Invoke(_d_get_workDir);
+                string save_dir = (string)this.Invoke(_get_workDir);
                 string save_filename = save_dir + projectID + "_" + site_name + " PID.txt";
 
                 List<string> fetch_data = new List<string>();
